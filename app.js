7b284292c88ba24fdb2d7a0588f62fd5cb97da6d
manishaac2002 <manishaac2002@gmail.com>
2023-08-25 12:11:11 +0530

    feat :testing port connection.

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 import express from "express";
 const app = express()
+const PORT =3000
 
 app.get('/', function (req, res) {
   res.send('Hello World....!')
 })
 
 app.listen(3000,()=>{
-  console.log("Server is running on port 3000");
+  console.log(`Server is running on port ${PORT}`);
 })
